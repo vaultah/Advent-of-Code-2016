df31f6d1116f99b4ae203f3c47774fbba0242938
vaultah <waultah@gmail.com>
2016-12-15 05:44:59 +0000

    Day 15

--- 15.py
+++ 15.py
@@ -7,8 +7,8 @@ inp = [(13, 10), (17, 15), (19, 17), (7, 1), (5, 0), (3, 1), (11, 0)]
 
 
 def sculpture(arg):
-    for start in count(1):
-        if any((pos + time + start) % n for time, (n, pos) in enumerate(arg, start=1)):
+    for start in count():
+        if any((pos + time + start + 1) % n for time, (n, pos) in enumerate(arg)):
             continue
 
         return start
